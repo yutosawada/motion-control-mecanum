b409310be58a59ad2ef9b5665d701a02513788de
Yuto Sawada <167935311+yutosawada@users.noreply.github.com>
2025-06-19 22:54:39 +0900

    Add test for MotionControllerNode

--- src/motion-control-mecanum/motion_controller_node.cpp
+++ src/motion-control-mecanum/motion_controller_node.cpp
@@ -9,89 +9,104 @@
 #include "sensor_msgs/msg/joint_state.hpp"
 
 namespace motion_control_mecanum {
+namespace {
 
-MotionControllerNode::MotionControllerNode(const rclcpp::NodeOptions& options)
-    : rclcpp::Node("motion_controller_node", options) {
-  this->declare_parameter("motors.FL_node_id", 1);
-  this->declare_parameter("motors.FR_node_id", 2);
-  this->declare_parameter("motors.RL_node_id", 3);
-  this->declare_parameter("motors.RR_node_id", 4);
-  this->declare_parameter("motor_parameters", rclcpp::ParameterValue());
-  this->declare_parameter("wheel_parameters", rclcpp::ParameterValue());
-  this->declare_parameter("control_parameters", rclcpp::ParameterValue());
-  this->declare_parameter("control_parameters.control_frequency", 50.0);
-  this->declare_parameter("control_parameters.max_linear_velocity_x", 1.0);
-  this->declare_parameter("control_parameters.max_linear_velocity_y", 1.0);
-  this->declare_parameter("control_parameters.max_angular_velocity", 1.5);
-  
+void initialize_node(MotionControllerNode * node,
+                     std::shared_ptr<MotionController> motion_controller) {
+  node->declare_parameter("motors.FL_node_id", 1);
+  node->declare_parameter("motors.FR_node_id", 2);
+  node->declare_parameter("motors.RL_node_id", 3);
+  node->declare_parameter("motors.RR_node_id", 4);
+  node->declare_parameter("motor_parameters", rclcpp::ParameterValue());
+  node->declare_parameter("wheel_parameters", rclcpp::ParameterValue());
+  node->declare_parameter("control_parameters", rclcpp::ParameterValue());
+  node->declare_parameter("control_parameters.control_frequency", 50.0);
+  node->declare_parameter("control_parameters.max_linear_velocity_x", 1.0);
+  node->declare_parameter("control_parameters.max_linear_velocity_y", 1.0);
+  node->declare_parameter("control_parameters.max_angular_velocity", 1.5);
+
   WheelParameters wheel_params;
   wheel_params.radius =
-      this->declare_parameter<double>("wheel_parameters.wheel_radius", 0.075);
+      node->declare_parameter<double>("wheel_parameters.wheel_radius", 0.075);
   wheel_params.separation_x =
-      this->declare_parameter<double>("wheel_parameters.wheel_separation_x", 0.30);
+      node->declare_parameter<double>("wheel_parameters.wheel_separation_x", 0.30);
   wheel_params.separation_y =
-      this->declare_parameter<double>("wheel_parameters.wheel_separation_y", 0.25);
-  
-      std::string can_dev =
-      this->declare_parameter<std::string>("can_device", "can0");
+      node->declare_parameter<double>("wheel_parameters.wheel_separation_y", 0.25);
+
+  std::string can_dev = node->declare_parameter<std::string>("can_device", "can0");
 
   MotorParameters motor_params;
   motor_params.acceleration =
-      this->declare_parameter<int>("motor_parameters.acceleration", 1000);
+      node->declare_parameter<int>("motor_parameters.acceleration", 1000);
   motor_params.deceleration =
-      this->declare_parameter<int>("motor_parameters.deceleration", 1000);
+      node->declare_parameter<int>("motor_parameters.deceleration", 1000);
   motor_params.max_torque =
-      this->declare_parameter<int>("motor_parameters.max_torque", 1000);
+      node->declare_parameter<int>("motor_parameters.max_torque", 1000);
   motor_params.end_velocity =
-      this->declare_parameter<int>("motor_parameters.end_velocity", 0);
-  motor_params.quick_stop_deceleration = this->declare_parameter<int>(
+      node->declare_parameter<int>("motor_parameters.end_velocity", 0);
+  motor_params.quick_stop_deceleration = node->declare_parameter<int>(
       "motor_parameters.quick_stop_deceleration", 1000);
   motor_params.velocity_window =
-      this->declare_parameter<int>("motor_parameters.velocity_window", 0);
+      node->declare_parameter<int>("motor_parameters.velocity_window", 0);
   motor_params.velocity_threshold =
-      this->declare_parameter<int>("motor_parameters.velocity_threshold", 0);
+      node->declare_parameter<int>("motor_parameters.velocity_threshold", 0);
 
   std::array<uint8_t, 4> node_ids{
-      static_cast<uint8_t>(this->get_parameter("motors.FL_node_id").as_int()),
-      static_cast<uint8_t>(this->get_parameter("motors.FR_node_id").as_int()),
-      static_cast<uint8_t>(this->get_parameter("motors.RL_node_id").as_int()),
-      static_cast<uint8_t>(this->get_parameter("motors.RR_node_id").as_int())};
-  
-  can_interface_ = std::make_shared<can_control::SocketCanInterface>(can_dev);
-  motion_controller_ = std::make_shared<MotionController>(
-      can_interface_, node_ids, motor_params, wheel_params);
+      static_cast<uint8_t>(node->get_parameter("motors.FL_node_id").as_int()),
+      static_cast<uint8_t>(node->get_parameter("motors.FR_node_id").as_int()),
+      static_cast<uint8_t>(node->get_parameter("motors.RL_node_id").as_int()),
+      static_cast<uint8_t>(node->get_parameter("motors.RR_node_id").as_int())};
 
-  control_params_.control_frequency =
-      this->get_parameter("control_parameters.control_frequency").as_double();
-  control_params_.max_linear_velocity_x =
-      this->get_parameter("control_parameters.max_linear_velocity_x")
-          .as_double();
-  control_params_.max_linear_velocity_y =
-      this->get_parameter("control_parameters.max_linear_velocity_y")
-          .as_double();
-  control_params_.max_angular_velocity =
-      this->get_parameter("control_parameters.max_angular_velocity")
-          .as_double();
+  if (!motion_controller) {
+    node->can_interface_ =
+        std::make_shared<can_control::SocketCanInterface>(can_dev);
+    node->motion_controller_ = std::make_shared<MotionController>(
+        node->can_interface_, node_ids, motor_params, wheel_params);
+  } else {
+    node->motion_controller_ = std::move(motion_controller);
+  }
 
-  cmd_vel_sub_ = create_subscription<geometry_msgs::msg::Twist>(
+  node->control_params_.control_frequency =
+      node->get_parameter("control_parameters.control_frequency").as_double();
+  node->control_params_.max_linear_velocity_x =
+      node->get_parameter("control_parameters.max_linear_velocity_x").as_double();
+  node->control_params_.max_linear_velocity_y =
+      node->get_parameter("control_parameters.max_linear_velocity_y").as_double();
+  node->control_params_.max_angular_velocity =
+      node->get_parameter("control_parameters.max_angular_velocity").as_double();
+
+  node->cmd_vel_sub_ = node->create_subscription<geometry_msgs::msg::Twist>(
       "cmd_vel", rclcpp::QoS(10),
-      std::bind(&MotionControllerNode::cmdVelCallback, this,
-                std::placeholders::_1));
+      std::bind(&MotionControllerNode::cmdVelCallback, node, std::placeholders::_1));
 
-  servo_on_service_ = create_service<std_srvs::srv::Trigger>(
-      "servo_on", std::bind(&MotionControllerNode::handleServoOn, this,
+  node->servo_on_service_ = node->create_service<std_srvs::srv::Trigger>(
+      "servo_on", std::bind(&MotionControllerNode::handleServoOn, node,
                             std::placeholders::_1, std::placeholders::_2));
 
-  servo_off_service_ = create_service<std_srvs::srv::Trigger>(
-      "servo_off", std::bind(&MotionControllerNode::handleServoOff, this,
+  node->servo_off_service_ = node->create_service<std_srvs::srv::Trigger>(
+      "servo_off", std::bind(&MotionControllerNode::handleServoOff, node,
                              std::placeholders::_1, std::placeholders::_2));
 
-  motor_state_pub_ =
-      create_publisher<sensor_msgs::msg::JointState>("motor_states", 10);
+  node->motor_state_pub_ =
+      node->create_publisher<sensor_msgs::msg::JointState>("motor_states", 10);
 
-  publish_timer_ = create_wall_timer(
-      std::chrono::duration<double>(1.0 / control_params_.control_frequency),
-      std::bind(&MotionControllerNode::publishMotorState, this));
+  node->publish_timer_ = node->create_wall_timer(
+      std::chrono::duration<double>(1.0 / node->control_params_.control_frequency),
+      std::bind(&MotionControllerNode::publishMotorState, node));
+}
+
+}  // namespace
+
+MotionControllerNode::MotionControllerNode(const rclcpp::NodeOptions & options)
+    : rclcpp::Node("motion_controller_node", options) {
+  initialize_node(this, nullptr);
+}
+
+MotionControllerNode::MotionControllerNode(
+    std::shared_ptr<MotionController> motion_controller,
+    const rclcpp::NodeOptions & options)
+    : rclcpp::Node("motion_controller_node", options) {
+  initialize_node(this, std::move(motion_controller));
 }
 
 void MotionControllerNode::cmdVelCallback(
