c56768835bab84a0131cf386a506dd86a11ddc66
Yuto Sawada <167935311+yutosawada@users.noreply.github.com>
2025-06-28 20:42:58 +0900

    Fix integration test linking

--- test/test_motion_controller_node.cpp
+++ test/test_motion_controller_node.cpp
@@ -86,11 +86,3 @@ TEST_F(MotionControllerNodeFixture, NoOdometryWhenComputeFails) {
   (void)sub;
 }
 
-int main(int argc, char ** argv) {
-  testing::InitGoogleTest(&argc, argv);
-  rclcpp::init(argc, argv);
-  int ret = RUN_ALL_TESTS();
-  rclcpp::shutdown();
-  return ret;
-}
-
